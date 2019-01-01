7fb7667b21efa2f99106f997dd1b1e8b5669e094
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-12-31 23:34:50 -0800

    time removed from class variables

--- rGAN.py
+++ rGAN.py
@@ -2,45 +2,51 @@ import torch
 from torch import nn
 
 class RecurrentGenerator(nn.Module):
-	#TODO
-	#work out shapes of input !
-	def __init__(self, time, num_nodes, d):
+	#input shape (seq_len, batch, input_size)
+	#output shape (seq_len, batch, input_size)
+	def __init__(self, num_nodes, d, num_layers=3, bidirectional=True):
 		super(RecurrentGenerator, self).__init__()
-		self.time = time
+		self.bidirectional = bidirectional
+		self.num_layers= num_layers
 		self.num_nodes = num_nodes
 		self.d = d
-		self.rnn = nn.LSTM(input_size=self.num_nodes, hidden_size=self.d, num_layers=3, bidirectional=True, nonlinearity="relu")
-		self.fc1 = nn.Sequential(
-			 nn.Linear(self.d, self.num_nodes),
-			 # nn.tanh()
-			 #TODO Find an activation that fits eeg generation
-		)
-
+		self.rnn1 = nn.LSTM(input_size=self.num_nodes, hidden_size=self.d, num_layers=self.num_layers, bidirectional=self.bidirectional)
+		self.rnn2 = nn.LSTM(input_size=self.d * (1 + (bidirectional*1)), hidden_size=self.num_nodes, num_layers=1, bidirectional=False)
+		# (seq_len, batch, num_directions * hidden_size)
+		# (num_layers * num_directions, batch, hidden_size)
 	def forward(self, x):
-		out = self.rnn(x)
-		out = self.fc1(out)
+		out, _ = self.rnn1(x)
+		out, _ = self.rnn2(out)
 		return out
 
-def RecurrentDiscriminator(nn.Module):
-	def __init__(self, time, num_nodes, d):
+class RecurrentDiscriminator(nn.Module):
+	#input shape (seq_len, batch, input_size)
+	#ourput shape (batch, 1)
+	def __init__(self, num_nodes, d, num_layers=3, bidirectional=True):
 		super(RecurrentDiscriminator, self).__init__()
-		self.time = time
+		self.bidirectional = bidirectional
+		self.num_layers= num_layers
 		self.num_nodes = num_nodes
 		self.d = d
-		self.rnn = nn.LSTM(input_size=self.num_nodes, hidden_size=self.d, num_layers=3, bidirectional=True, nonlinearity="relu")
+		self.rnn1 = nn.LSTM(input_size=self.num_nodes, hidden_size=self.d, num_layers=self.num_layers, bidirectional=self.bidirectional)
+		self.rnn2 = nn.LSTM(input_size=self.d * (1 + (bidirectional*1)), hidden_size=self.num_nodes, num_layers=1, bidirectional=False)
 		self.fc1 = nn.Sequential(
-			nn.Linear(self.d * self.time, self.d),
+			nn.Linear(self.num_nodes * 1, self.num_nodes//2),
 			nn.ReLU()
 		)
 		self.fc2 = nn.Sequential(
-			nn.Linear((self.d), 1),
+			nn.Linear(self.num_nodes//2, 1),
 			nn.Sigmoid()
 		)
 
 	def forward(self, x):
-		out = self.rnn(x)
-		out = out.view(-1, self.num_nodes, self.d*self.time)
+		output, (hn, cn) = self.rnn1(x)
+		out = output
+		output, (hn, cn) = self.rnn2(out) 
+		out = cn #TODO WHICH ONE
+		out = out.view(-1, self.num_nodes * 1)
 		out = self.fc1(out)
 		out = self.fc2(out)
 		return out
+
 
