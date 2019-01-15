cd39587c0aed68190ca67a763ee9b0b9accb8ee7
DanielLongo <danieldominiclongo@gmail.com>
2019-01-14 21:05:42 -0800

    not working on remote server

---
+++ add_noise.py
@@ -0,0 +1,135 @@
+import torch
+from rGAN import RecurrentGenerator, RecurrentDiscriminator
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from load_EEGs import EEGDataset
+
+use_cuda = False
+ITERS = 10
+CRITIC_ITERS = 3
+BATCH_SIZE = 4
+LAMBDA = 10 # Gradient penalty lambda hyperparameter
+NUM_NODES= 44
+
+# noisy_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES) #None, batch_size, time, num_nodes
+noisy_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
+# clean_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES)
+clean_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
+
+
+one = torch.FloatTensor([1])
+# one = [torch.FloatTensor(1).cuda() for _ in range(1)]
+mone = one * -1
+if use_cuda:
+	one = one.cuda()
+	mone = mone.cuda()
+
+netD = RecurrentDiscriminator(NUM_NODES, 64)
+netG = RecurrentGenerator(num_nodes=NUM_NODES, d=50)
+
+optimizerD = optim.Adam(netD.parameters(), lr=1e-4, betas=(0.5, 0.9))
+optimizerG = optim.Adam(netG.parameters(), lr=1e-4, betas=(0.5, 0.9))
+
+def calc_gradient_penalty(netD, real_data, fake_data):
+	# print(real_data.size())
+	alpha = torch.rand(BATCH_SIZE, 1, 1)
+	alpha = alpha.expand(real_data.size())
+	# print("alpha", alpha.size())
+	alpha = alpha.cuda() if use_cuda else alpha
+
+	interpolates = alpha * real_data + ((1 - alpha) * fake_data)
+
+	if use_cuda:
+		interpolates = interpolates.cuda()
+	interpolates = autograd.Variable(interpolates, requires_grad=True)
+
+	disc_interpolates = netD(interpolates)
+
+	gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+							  grad_outputs=torch.ones(disc_interpolates.size()).cuda() if use_cuda else torch.ones(
+								  disc_interpolates.size()),
+							  create_graph=True, retain_graph=True, only_inputs=True)[0]
+
+	gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
+	return gradient_penalty
+
+def main():
+	for iteration in range(ITERS):
+		# for i in range(noisy_eegs.shape[0]):
+		for i in range(len(noisy_eegs)):
+			############################
+			# (1) Update D network
+			###########################
+			for p in netD.parameters():  # reset requires_grad
+				p.requires_grad = True  # they are set to False below in netG update
+			# for p in netG.parameters():
+			# 	p.requires_grad = False
+
+			for iter_d in range(CRITIC_ITERS):
+				real_data = noisy_eegs[i]
+				real_data_v = autograd.Variable(real_data)
+				if use_cuda:
+					real_data = real_data.cuda()
+
+				netD.zero_grad()
+
+				# train with real
+			D_real = netD(real_data_v)
+			D_real = D_real.mean()
+			# print D_real
+			# D_real.backward(mone)
+			D_real.backward()
+
+			# train with fake
+			fake_data = clean_eegs[i]
+			if use_cuda:
+				fake_data = fake_data.cuda()
+			fake_datav = autograd.Variable(fake_data, volatile=True)  # totally freeze netG
+			fake_data.requires_grad = False
+			# fake_data.requires_grad = True
+			# fake_datav = fake_data
+			fake = autograd.Variable(netG(fake_datav).data)
+			inputv = fake
+			# inputv = fake_data
+			D_fake = netD(inputv)
+			D_fake = D_fake.mean()
+			# D_fake.backward(one)
+			D_fake.backward()
+
+
+			# train with gradient penalty
+			gradient_penalty = calc_gradient_penalty(netD, real_data_v.data, fake.data)
+			gradient_penalty.backward()
+
+			D_cost = D_fake - D_real #+ gradient_penalty
+			Wasserstein_D = D_real - D_fake
+			optimizerD.step()
+
+		############################
+		# (2) Update G network
+		###########################
+		for p in netD.parameters():
+			p.requires_grad = False  # to avoid computation
+		# for p in netG.parameters():
+		# 	p.requires_grad = True
+		netG.zero_grad()
+
+		fake_data = clean_eegs[i]
+		if use_cuda:
+			fake_data = fake_data.cuda()
+		fake_datav = autograd.Variable(fake_data)
+		fake = autograd.Variable(netG(fake_datav).data)
+		G = netD(fake)
+		G.requires_grad = True
+		G = G.mean()
+		# G.backward(mone)
+		G.backward()
+		G_cost = -G
+		optimizerG.step()
+
+		print("G_cost" , G_cost)
+		print("D_cost", D_cost)
+if __name__ == "__main__":
+	main()
---
+++ load_EEGs.py
@@ -0,0 +1,119 @@
+import h5py
+import time
+import datetime
+import numpy as np
+import os
+import torch
+from torch.utils import data
+import h5py
+
+class EEGDataset(data.Dataset):
+	def __init__(self, data_dir, num_channels=19, num_examples=-1, batch_size=64):
+		self.examples_signal, self.examples_atribute = load_eeg_directory(data_dir, num_channels, min_length=100, max_length=999999, max_num=num_examples)
+		print("before", np.shape(self.examples_signal))
+		self.batched_examples_atribute = split_into_batches(self.examples_atribute, batch_size)
+		self.batched_examples_signal = split_into_batches(self.examples_signal, batch_size)
+		print("after", np.shape(self.batched_examples_signal))
+
+	def __len__(self):
+		return len(self.batched_examples_atribute)
+
+	def __getitem__(self, index):
+		# Select sample
+		print(np.shape(self.batched_examples_signal))
+		print("to be sent", np.asarray(self.batched_examples_signal[index]).shape)
+		batch = self.batched_examples_signal[index]
+		sample = torch.from_numpy(np.asarray(batch))
+		sample = sample.view(-1, sample.shape[2], sample.shape[1]).type('torch.FloatTensor')
+		# sample = torch.from_numpy((self.batched_examples_signal[index]))#, self.examples_atribute[index]
+		print(sample.shape)
+		return sample
+
+
+def load_eeg_file(filename):
+	hdf = h5py.File(filename, "r")
+	atributes = hdf["patient"].attrs
+	rec = hdf["record-0"]
+	signals = rec["signals"]
+	atributes = parse_atributes(atributes)
+	specs = {
+		"sample_frequency" : rec.attrs["sample_frequency"],
+		"number_channels" : rec.attrs["number_channels"]
+	}
+	return signals, atributes, specs
+
+def parse_atributes(atributes):
+	gender = atributes["gender"]
+	if gender == "Male":
+		gender = 1
+	elif gender == "Female":
+		gender = -1
+	else:
+		gender = 0
+	gestatational_age_at_birth_days = float(atributes["gestatational_age_at_birth_days"])
+	birthdate = atributes["birthdate"]
+	age_in_seconds = (time.time() - time.mktime(datetime.datetime.strptime(birthdate, "%Y-%m-%d").timetuple()))
+	# print("gender", gender, type(gender))
+	# print("gestatational_age_at_birth_days", gestatational_age_at_birth_days, type(gestatational_age_at_birth_days))
+	# print("age_in_seconds", age_in_seconds, type(age_in_seconds))
+	out = np.asarray([gender, age_in_seconds, gestatational_age_at_birth_days])
+	return out
+	#todo, figure out born premature
+	# born_premature = atributes["born_premature"]
+
+
+def load_eeg_directory(path, num_channels, min_length=0, max_length=1e9999, max_num=-1, sample_frequency=200, length=10000):
+	files = os.listdir(path)
+	num_files_read = 0
+	examples_signal = []
+	examples_atribute = []
+	for file in files:
+		if file.split(".")[-1] != "eeghdf":
+			continue
+		signals, atributes, specs = load_eeg_file(path + file)
+		# print(signals.shape[1])
+		signals = signals[:, :length]
+		if signals.shape[1] != length:
+			continue
+		if (int(specs["number_channels"]) != num_channels):
+			# print("Not correct num_channels", num_channels, specs["number_channels"])
+			continue
+		if (int(specs["sample_frequency"]) != sample_frequency):
+			# print("Not correct sample_frequency", sample_frequency, specs["sample_frequency"])
+			continue
+		num_readings = signals.shape[1]
+		# if num_readings < min_length:
+		# 	continue
+		# elif num_readings > max_length:
+		# 	continue 
+		examples_signal += [signals]
+		examples_atribute += [atributes]
+		
+		if num_files_read-1 == max_num:
+			return examples_signal, examples_atribute
+		num_files_read += 1
+
+	return examples_signal, examples_atribute
+
+def split_into_batches(x, examples_per_batch):
+	final = []
+	for start in range(0, len(x), examples_per_batch):
+		end = start + examples_per_batch
+		final += [x[start:end]]
+	print("final", np.shape(final))
+	return final
+
+
+# dataset = Dataset("./eeg-hdfstorage/data/")
+if __name__ == "__main__":
+	dataset = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=44)
+# filename = "./eeg-hdfstorage/data/absence_epilepsy.eeghdf"
+# # signals, atributes = load_eeg_file(filename)
+# # print("atributes", atributes.shape)
+# signals, atributes = load_eeg_directory("./eeg-hdfstorage/data/", 19)
+# print(np.shape(signals))
+# print(np.shape(atributes))
+
+# print(signals.shape)
+# print(list(atributes.items()))
+# print(atributes["patient_name"])
