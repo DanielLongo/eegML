16791a3528a44d645761106a958f9610ed7fd4e8
Daniel Longo <danieldominiclongo@gmail.com>
2019-01-17 21:44:48 -0800

    trains

--- add_noise.py
+++ add_noise.py
@@ -4,96 +4,55 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-<<<<<<< HEAD
-
-use_cuda = False
-=======
 from load_EEGs import EEGDataset
 
 use_cuda = True
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 ITERS = 10
 CRITIC_ITERS = 3
-BATCH_SIZE = 4
+BATCH_SIZE = 64
 LAMBDA = 10 # Gradient penalty lambda hyperparameter
-<<<<<<< HEAD
-
-noisy_eegs = torch.randn(16, BATCH_SIZE, 100, 32) #None, batch_size, time, num_nodes
-clean_eegs = torch.randn(16, BATCH_SIZE, 100, 32)
-
-
-one = torch.FloatTensor([1])
-mone = one * -1
-if use_cuda:
-	one = one.cuda(gpu)
-	mone = mone.cuda(gpu)
-
-netD = RecurrentDiscriminator(32, 64)
-netG = RecurrentGenerator(num_nodes=32, d=50)
-=======
 NUM_NODES= 44
 
-noisy_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES) #None, batch_size, time, num_nodes
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-# noisy_eegs = EEGDataset("./the_server/Users/DanielLongo/server/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
-clean_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES)
-# clean_eegs = EEGDataset("./the_server/Users/DanielLongo/server/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
-=======
-# noisy_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
-clean_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES)
-# clean_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
->>>>>>> Stashed changes
-=======
-# noisy_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
-clean_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES)
-# clean_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=10, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
->>>>>>> Stashed changes
+# noisy_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES) #None, batch_size, time, num_nodes
+# clean_eegs = torch.randn(16, BATCH_SIZE, 100, NUM_NODES)
+clean_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=512, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
+noisy_eegs = EEGDataset("/mnt/data1/eegdbs/SEC-0.1/stanford/", num_examples=512, num_channels=NUM_NODES, batch_size=BATCH_SIZE)
 
 
-one = torch.FloatTensor([1])
-# one = [torch.FloatTensor(1).cuda() for _ in range(1)]
+# one = torch.FloatTensor([1]).cuda()
+one = torch.ones([]).cuda()
+# one = [torch.FloatTensor([1.0]).cuda() for _ in range(1)]
 mone = one * -1
-if use_cuda:
-	one = one.cuda()
-	mone = mone.cuda()
+# if use_cuda:
+	# one = one.cuda()
+	# mone = mone.cuda()
 
 netD = RecurrentDiscriminator(NUM_NODES, 64)
 netG = RecurrentGenerator(num_nodes=NUM_NODES, d=50)
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
+
+netD.cuda()
+netG.cuda()
 
 optimizerD = optim.Adam(netD.parameters(), lr=1e-4, betas=(0.5, 0.9))
 optimizerG = optim.Adam(netG.parameters(), lr=1e-4, betas=(0.5, 0.9))
 
 def calc_gradient_penalty(netD, real_data, fake_data):
-	# print(real_data.size())
+	print(real_data.size())
 	alpha = torch.rand(BATCH_SIZE, 1, 1)
 	alpha = alpha.expand(real_data.size())
-	# print("alpha", alpha.size())
-<<<<<<< HEAD
-	alpha = alpha.cuda(gpu) if use_cuda else alpha
-=======
+	print("alpha", alpha.size())
 	alpha = alpha.cuda() if use_cuda else alpha
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 
 	interpolates = alpha * real_data + ((1 - alpha) * fake_data)
 
 	if use_cuda:
-<<<<<<< HEAD
-		interpolates = interpolates.cuda(gpu)
-=======
 		interpolates = interpolates.cuda()
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 	interpolates = autograd.Variable(interpolates, requires_grad=True)
 
 	disc_interpolates = netD(interpolates)
 
 	gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-<<<<<<< HEAD
-							  grad_outputs=torch.ones(disc_interpolates.size()).cuda(gpu) if use_cuda else torch.ones(
-=======
 							  grad_outputs=torch.ones(disc_interpolates.size()).cuda() if use_cuda else torch.ones(
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 								  disc_interpolates.size()),
 							  create_graph=True, retain_graph=True, only_inputs=True)[0]
 
@@ -102,12 +61,8 @@ def calc_gradient_penalty(netD, real_data, fake_data):
 
 def main():
 	for iteration in range(ITERS):
-<<<<<<< HEAD
-		for i in range(noisy_eegs.shape[0]):
-=======
 		# for i in range(noisy_eegs.shape[0]):
 		for i in range(len(noisy_eegs)):
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 			############################
 			# (1) Update D network
 			###########################
@@ -118,44 +73,32 @@ def main():
 
 			for iter_d in range(CRITIC_ITERS):
 				real_data = noisy_eegs[i]
-				real_data_v = autograd.Variable(real_data)
+
 				if use_cuda:
-<<<<<<< HEAD
-					real_data = real_data.cuda(gpu)
-=======
 					real_data = real_data.cuda()
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
+				real_data_v = autograd.Variable(real_data)
 
 				netD.zero_grad()
 
-				# train with real
+			# train with real
 			D_real = netD(real_data_v)
 			D_real = D_real.mean()
 			# print D_real
-<<<<<<< HEAD
+			# print(D_real)
+			# print(mone)
 			D_real.backward(mone)
-=======
-			#D_real.backward(mone)
-			D_real.backward()
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
+			# D_real.backward()
 
 			# train with fake
 			fake_data = clean_eegs[i]
 			if use_cuda:
-<<<<<<< HEAD
-				fake_data = noise.cuda(gpu)
-			# fake_datav = autograd.Variable(fake_data, volatile=True)  # totally freeze netG
-			fake_data.requires_grad = False
-			fake_datav = fake_data
-			fake = autograd.Variable(netG(fake_datav).data)
-			inputv = fake
-			D_fake = netD(inputv)
-			D_fake = D_fake.mean()
-			D_fake.backward(one)
-=======
 				fake_data = fake_data.cuda()
-			fake_datav = autograd.Variable(fake_data, volatile=True)  # totally freeze netG
-			fake_data.requires_grad = False
+			# fake_datav = autograd.Variable(fake_data, volatile=True)  # depricated version
+
+			fake_datav = autograd.Variable(fake_data)  # totally freeze netG
+			fake_datav.requires_grad = False
+
+
 			# fake_data.requires_grad = True
 			# fake_datav = fake_data
 			fake = autograd.Variable(netG(fake_datav).data)
@@ -163,14 +106,13 @@ def main():
 			# inputv = fake_data
 			D_fake = netD(inputv)
 			D_fake = D_fake.mean()
-			# D_fake.backward(one)
-			D_fake.backward()
+			D_fake.backward(one)
+			# D_fake.backward()
 
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 
 			# train with gradient penalty
-			gradient_penalty = calc_gradient_penalty(netD, real_data_v.data, fake.data)
-			gradient_penalty.backward()
+			#gradient_penalty = calc_gradient_penalty(netD, real_data_v.data, fake.data)
+			#gradient_penalty.backward()
 
 			D_cost = D_fake - D_real #+ gradient_penalty
 			Wasserstein_D = D_real - D_fake
@@ -187,30 +129,18 @@ def main():
 
 		fake_data = clean_eegs[i]
 		if use_cuda:
-<<<<<<< HEAD
-			fake_data = noise.cuda(gpu)
-=======
 			fake_data = fake_data.cuda()
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
 		fake_datav = autograd.Variable(fake_data)
-		fake = autograd.Variable(netG(fake_datav).data)
+		fake = netG(fake_datav)
 		G = netD(fake)
-		G.requires_grad = True
+		# G.requires_grad = True
 		G = G.mean()
-<<<<<<< HEAD
 		G.backward(mone)
-=======
-		# G.backward(mone)
-		G.backward()
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
+		# G.backward()
 		G_cost = -G
 		optimizerG.step()
 
 		print("G_cost" , G_cost)
 		print("D_cost", D_cost)
 if __name__ == "__main__":
-<<<<<<< HEAD
 	main()
-=======
-	main()
->>>>>>> 3ebe3bddf86ebb19c53395ddfff66b9644a1b327
