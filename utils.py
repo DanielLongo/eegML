181966587b56b8f530007eacafef015ad226c55b
Daniel Longo <dlongo@lotus.lee-messer.net>
2019-01-19 17:09:18 -0800

    first

---
+++ utils.py
@@ -0,0 +1,8 @@
+import numpy as np 
+def save_EEG(data,num_channels, frequency, filename, channel_names=None):
+	np.save(filename, data)
+	# channel_names = list(range(num_channels)) if (channel_names==None) else channel_names
+	# info = mne.create_info(channel_names, frequency, 
+                       # "eeg")
+	# customraw = mne.io.RawArray(data, info)
+	# customraw.save(filename, overwrite=True)
