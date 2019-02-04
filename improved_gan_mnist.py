c68db9c07b5dd8f697464575dedbf54c57bba1db
Daniel Longo <dlongo@lotus.lee-messer.net>
2019-02-04 00:51:21 -0800

    grad issues fixed

--- improved_gan_mnist.py
+++ improved_gan_mnist.py
@@ -95,10 +95,10 @@ class Generator(nn.Module):
 		return out
 
 
-	def weight_init(m, mean, std):
-		if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
-			m.weight.data.normal_(mean, std)
-			m.bias.data.zero_()
+	# def weight_init(m, mean, std):
+	# 	if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+	# 		m.weight.data.normal_(mean, std)
+	# 		m.bias.data.zero_()
 
 def create_optimizer(model, lr=.01, betas=None):
 	if betas == None:
@@ -128,11 +128,14 @@ def save_images_func(generator, epoch, i, filename_prefix):
 
 def train_gan(discriminator, generator, image_loader, num_epochs, batch_size, g_lr, d_lr, dtype, filename_prefix="DCGAN-", save_images=True):
 	iters = 0
-	optimizerD = torch.optim.Adam(discriminator.parameters(), lr=1e-4, betas=(0.5, 0.999))
-	optimizerG = torch.optim.Adam(generator.parameters(), lr=1e-4, betas=(0.5, 0.999))
+	optimizerD = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+	optimizerG = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
 	criterionD = nn.CrossEntropyLoss().cuda() # binary cross-entropy
 	criterionG = nn.MSELoss().cuda()
 	iters = 0
+	BCE = nn.BCELoss()
+	real_labels = autograd.Variable(torch.ones(batch_size).type(dtype))
+	fake_labels = autograd.Variable(torch.zeros(batch_size).type(dtype))
 	for epoch in range(num_epochs):
 		for x, _ in image_loader:
 			if (x.shape[0] != batch_size):
@@ -141,67 +144,62 @@ def train_gan(discriminator, generator, image_loader, num_epochs, batch_size, g_
 			############################
 			# (1) Update D network
 			###########################
-			for p in discriminator.parameters():  # reset requires_grad
-				p.requires_grad = True  # they are set to False below in discriminator update:
-
+			# for p in discriminator.parameters():  # reset requires_grad
+				# p.requires_grad = True  # they are set to False below in discriminator update:
+#
 			# for p in discriminator.parameters():
-			# 	p.requires_grad = False # to avoid computation
+				# p.requires_grad = False # to avoid computation
 			
 			discriminator.zero_grad()
-			inputv = autograd.Variable(x)
-
-			inputv = inputv.type(dtype)
-
-			# print("inputv", inputv.shape)
-
-			unl_output = discriminator(inputv)
-			loss_unl_real = -torch.mean(LSE(unl_output),0) +  torch.mean(F.softplus(LSE(unl_output),1),0)
-			real_loss = unl_output
+			generator.zero_grad()
+			inputv = autograd.Variable(x).type(dtype)
+			d_real = discriminator(inputv)
 
 			# train with fake
-			# noise = discriminator.generate_noise(batch_size, LENGTH, NUM_NODES)
-			noise = generate_nosie(x.shape[0])
-
-			noise = noise.type(dtype)
-
+			noise = generate_nosie(x.shape[0]).type(dtype)
 			noise_v = autograd.Variable(noise)
+			fake = generator(noise_v)
+			# d_fake = discriminator(fake.detach()) #fake images are separated from the graph #results will never gradient(be updated), so G will not be updated
+			d_fake = discriminator(fake) 
 
-			fake = autograd.Variable(generator(noise_v).data)
-			unl_output = discriminator(fake.detach()) #fake images are separated from the graph #results will never gradient(be updated), so G will not be updated
-			loss_unl_fake = torch.mean(F.softplus(LSE(unl_output),1),0)
-			fake_loss = unl_output
 			# loss_D = loss_unl_real + loss_unl_fake
 			# loss_D = real_loss - fake_loss
-			loss_D = torch.mean(real_loss) - torch.mean(fake_loss)
+
+			loss_real = BCE(d_real, real_labels)
+			loss_fake = BCE(d_fake, fake_labels)
+
+			# Optimize discriminator
+			loss_D = loss_real + loss_fake
 			loss_D.backward()# because detach(), backward() will not influence discriminator
 			optimizerD.step()
 
 		############################
 		# (2) Update G network
 		###########################
-			for p in discriminator.parameters():
-				p.requires_grad = False  # to avoid computation
+			# for p in discriminator.parameters():
+			# 	p.requires_grad = False  # to avoid computation
 
 			# for p in discriminator.parameters():
 			# 	p.requires_grad = True
 
 			discriminator.zero_grad()
+			generator.zero_grad()
 
 			# noise = discriminator.generate_noise(batch_size, LENGTH, NUM_NODES)
-			noise = generate_nosie(x.shape[0])
-
-			noise = noise.type(dtype)
+			noise = generate_nosie(x.shape[0]).type(dtype)
 
 			noise_v = autograd.Variable(noise)
 
-			fake = autograd.Variable(generator(noise_v).data)
+			fake = generator(noise_v)
 			
-			inputv = autograd.Variable(x).type(dtype)
-			feature_real,_ = discriminator(inputv, matching=True)
-			feature_fake,output = discriminator(fake, matching=True)
-			feature_real = torch.mean(feature_real,0)
-			feature_fake = torch.mean(feature_fake,0)
-			loss_G = criterionG(feature_fake, feature_real.detach())
+			d_fake_ = discriminator(fake)
+			loss_G = BCE(d_fake_, real_labels)
+			loss_G.backward()
+			# feature_real,_ = discriminator(inputv, matching=True)
+			# feature_fake,output = discriminator(fake, matching=True)
+			# feature_real = torch.mean(feature_real,0)
+			# feature_fake = torch.mean(feature_fake,0)
+			# loss_G = criterionG(feature_fake, feature_real.detach())
 			optimizerG.step()
 			# if (iters % 1000 == 0):
 			# 	save_EEG(fake.cpu().detach().numpy(), NUM_NODES, 200, "./generated_eegs/generated-iter"+ str(iters) + "-fake-rG-long")
