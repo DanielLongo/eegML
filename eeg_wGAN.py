ea665e8209d3b25bd899e18faf14b4ccad5c409e
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-02-08 10:05:04 -0800

    convert to tabs

--- eeg_wGAN.py
+++ eeg_wGAN.py
@@ -61,25 +61,25 @@ if cuda:
 
 # Initialize weights
 def compute_gradient_penalty(D, real_samples, fake_samples):
-    """Calculates the gradient penalty loss for WGAN GP"""
-    # Random weight term for interpolation between real and fake samples
-    alpha = Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
-    # Get random interpolation between real and fake samples
-    interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
-    d_interpolates = D(interpolates)
-    fake = Variable(Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
-    # Get gradient w.r.t. interpolates
-    gradients = autograd.grad(
-        outputs=d_interpolates,
-        inputs=interpolates,
-        grad_outputs=fake,
-        create_graph=True,
-        retain_graph=True,
-        only_inputs=True,
-    )[0]
-    gradients = gradients.view(gradients.size(0), -1)
-    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
-    return gradient_penalty
+	"""Calculates the gradient penalty loss for WGAN GP"""
+	# Random weight term for interpolation between real and fake samples
+	alpha = Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+	# Get random interpolation between real and fake samples
+	interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
+	d_interpolates = D(interpolates)
+	fake = Variable(Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+	# Get gradient w.r.t. interpolates
+	gradients = autograd.grad(
+		outputs=d_interpolates,
+		inputs=interpolates,
+		grad_outputs=fake,
+		create_graph=True,
+		retain_graph=True,
+		only_inputs=True,
+	)[0]
+	gradients = gradients.view(gradients.size(0), -1)
+	gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+	return gradient_penalty
 # generator.apply(weights_init_normal)
 # discriminator.apply(weights_init_normal)
 
@@ -122,7 +122,7 @@ for epoch in range(n_epochs):
 		# Loss measures generator's ability to fool the discriminator
 		gradient_penalty = compute_gradient_penalty(discriminator, real_imgs.data, gen_imgs.data)
 		real_validity = discriminator(real_imgs)
-                fake_validity = discriminator(gen_imgs)
+				fake_validity = discriminator(gen_imgs)
 		d_loss = -torch.mean(real_validity) + torch.mean(fake_validity) + lambda_gp * gradient_penalty
 		#g_loss = adversarial_loss(discriminator(gen_imgs), valid)
 
@@ -139,7 +139,7 @@ for epoch in range(n_epochs):
 		fake_imgs = generator(z)
 
 		fake_validity = discriminator(fake_imgs)
- 		g_loss = -torch.mean(fake_validity)
+		g_loss = -torch.mean(fake_validity)
 
 		g_loss.backward()
 		optimizer_G.step()
